f14c8e762c80d616a11daee71339333dffb84821
Bhavesh Kumar <er.bhavesh@gmail.com>
2020-07-06 22:49:20 +0530

    add main method

--- ClubHouse/ClubHouse.py
+++ ClubHouse/ClubHouse.py
@@ -1,48 +1,57 @@
 # -*- coding: utf-8 -*-
 from Applicant import Applicant
-from HashTables import HashTable
+from HashTable import HashTable
 from Status import Status
 
-p1 = Applicant("Bhavesh", "9619655828","ref1",Status.Applied)
-p2 = Applicant("Kumar bhavesh", "9619655828","ref1",Status.Applied)
-p3 = Applicant("Bhavesh1", "9619655828","ref1",Status.Applied)
-p4 = Applicant("Kumar1 bhavesh", "9619655828","ref1",Status.Applied)
-
-h1=HashTable()
-
-h1.put(p1.name,p1)
-
-h1.put(p2.name,p2)
-h1.put(p3.name,p3)
-
-h1.put(p4.name,p4)
-
-print("Get Value >>>>>>")
-
-print("Key Doesnt exist ", h1.get("abc"))
-print(h1.get("Bhavesh").phoneNumber)
-
-print(h1.keys())
-print(repr(h1.values()))
-print("Exit *** ")
-
-def initializeHash(self): 
-    self.ApplicationRecords=HashTable()
-
-def insertAppDetails(self,ApplicationRecords, name, phone, memRef, status):
-         p1 = Applicant(name, phone,memRef,status)
-         self.ApplicationRecords.put(p1.name,p1)
-         
-def updateAppDetails(self,ApplicationRecords, name, phone, memRef, status):
-         p1 = Applicant(name, phone,memRef,status)
-         self.ApplicationRecords.put(p1.name,p1)
-                 
-def memRef(self,ApplicationRecords, memID): 
-    return
-def appStatus(self,ApplicationRecords):
-    return
-
-def destroyHash(self,ApplicationRecords): 
-    self.ApplicationRecords
+class ClubHouse :
+    def __init__(self):
+        print("Hi")
+        
+    def initializeHash(self): 
+        self.ApplicationRecords=HashTable()
+    
+    def insertAppDetails(self, name, phone, memRef, status):
+             p1 = Applicant(name, phone,memRef,status)
+             self.ApplicationRecords.put(p1.name,p1)
+             
+    def updateAppDetails(self, name, phone, memRef, status):
+             p1 = Applicant(name, phone,memRef,status)
+             self.ApplicationRecords.put(p1.name,p1)
+                     
+    def memRef(self, memRefId): 
+        
+        res = [] 
+        for val in self.ApplicationRecords.values(): 
+            if val.memberReferenceId== memRefId : 
+                res.append(val)
+        print(res)    
+        return res
+        
+    def appStatus(self):
+        res = [] 
+        
+            
+        return res
+    
+    def destroyHash(self,ApplicationRecords): 
+        self.ApplicationRecords
+        
+        
+def main():
+    c=ClubHouse()
+    c.initializeHash()
+    c.insertAppDetails("Bhavesh", "9619655828","ref1","Status.Applied")
+    c.insertAppDetails("Bhavesh1", "9619655828","ref1","Status.Applied")
+    c.insertAppDetails("Bhavesh2", "9619655828","ref1","Status.Applied")
+    c.insertAppDetails("Bhavesh3", "9619655828","ref1","Status.Applied")
+    c.insertAppDetails("Bhavesh4", "9619655828","ref2","Status.Applied")
+    c.insertAppDetails("Bhavesh5", "9619655828","ref2","Status.Applied")
+    print("hello world!")
+    c.memRef("ref2")
+    c.updateAppDetails("Bhavesh5", "9619655829","ref2","Status.Applied")
+    c.memRef("ref2")
+    
+if __name__ == "__main__":
+    main()   
     
          
